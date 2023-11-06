aa8879f25d8ccae47891c6b934a56c92a4daff1d
Sourav <sourav@ndtvs-MacBook-Air.local>
2023-11-06 14:27:25 +0530

    added ssr

--- src/app/blog/[blogpost]/page.tsx
+++ src/app/blog/[blogpost]/page.tsx
@@ -1,43 +1,51 @@
-"use client";
-import { useEffect, useState } from "react";
+// "use client";
+// import { useEffect, useState } from "react";
 import styles from "@/styles/blogpost.module.css";
-import { useRouter } from "next/router";
 
-export default function Page({ params }: { params: { blogpost: string } }) {
-  const [blogTitle, setBlogTitle] = useState();
-  const [blogAuthor, setBlogAuthor] = useState();
-  const [blogContent, setBlogContent] = useState();
+export default async function Page({ params }: { params: { blogpost: string } }) {
+  const fetchData = async () => {
+    const blogs = await fetch(`http://localhost:3000/api/getblog?blogpost=${params.blogpost}`).then(
+      (response) => response.json()
+    );
+    return blogs.message
+  };
+  
+  const blogs= await fetchData()
+  
+  // const [blogTitle, setBlogTitle] = useState();
+  // const [blogAuthor, setBlogAuthor] = useState();
+  // const [blogContent, setBlogContent] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:3000/api/getblog?blogpost=${params.blogpost}`
-        );
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setBlogTitle(data.message.title);
-        setBlogContent(data.message.content);
-        setBlogAuthor(data.message.author)
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `http://localhost:3000/api/getblog?blogpost=${params.blogpost}`
+  //       );
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const data = await response.json();
+  //       setBlogTitle(data.message.title);
+  //       setBlogContent(data.message.content);
+  //       setBlogAuthor(data.message.author)
 
-      } catch (error) {
-        console.error("Some error occurred", error);
-      }
-    };
+  //     } catch (error) {
+  //       console.error("Some error occurred", error);
+  //     }
+  //   };
 
-    fetchData();
-  }, [blogTitle]);
+  //   fetchData();
+  // }, [blogTitle]);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <>
-          <h1 className="p-2 ">{blogTitle}</h1>
+          <h1 className="p-2 ">{blogs.title}</h1>
           <hr />
-          <h3>Author: {blogAuthor}</h3>
-          <div className="p-2">{blogContent}</div>
+          <h3>Author: {blogs.author}</h3>
+          <div className="p-2">{blogs.content}</div>
         </>
       </main>
     </div>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+// "use client"
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import styles from "@/styles/blog.module.css";
@@ -46,12 +47,4 @@ const blogs= await fetchData()
     </main>
   );
 };
-// export async function getServerSideProps() {
-//   // Fetch data from external API
-//   // const res = await fetch(`https://.../data`)
-//   // const data = await res.json()
-
-//   // Pass data to the page via props
-//   return { props: { data } }
-// }
 export default Blog;
